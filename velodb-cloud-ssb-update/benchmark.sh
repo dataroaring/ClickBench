68f7534b23d20cc56510f657ad1ada7c192fc029
Yongqiang YANG <yangyogqiang@selectdb.com>
2025-07-22 21:07:42 +0800

    add velodb-cloud-ssb-update

---
+++ velodb-cloud-ssb-update/benchmark.sh
@@ -0,0 +1,87 @@
+#!/bin/bash
+set -e
+
+export VELODB_ENDPOINT=${VELODB_ENDPOINT:-"http://localhost:8030"}
+export VELODB_USER=${VELODB_USER:-"root"}
+export VELODB_PASSWORD=${VELODB_PASSWORD:-""}
+export VELODB_PORT=${VELODB_PORT:-"9030"}
+export S3_AK=
+export S3_SK=
+export PERCENTAGE=${PERCENTAGE:-25}
+
+ROOT=$(pwd)
+
+export MYSQL_CMD="mysql -vvv -h${VELODB_ENDPOINT} -p${VELODB_PASSWORD} -P${VELODB_PORT} -u${VELODB_USER}"
+
+${MYSQL_CMD} -e 'create database if not exists ssb'
+${MYSQL_CMD} ssb <"$ROOT"/create.sql
+
+# Load data
+echo "start loading, estimated to take about 9 minutes ..."
+
+date
+START=$(date +%s)
+
+${MYSQL_CMD} ssb -e "
+    INSERT INTO customer SELECT * FROM s3('uri' = 's3://yyq-test/regression/ssb/sf100/customer.tbl.gz',
+            's3.access_key'= '${S3_AK}',
+            's3.secret_key' = '${S3_SK}',
+            's3.endpoint' = 's3.us-west-2.amazonaws.com',
+            's3.region' = 'us-west-2',
+            'format' = 'csv');
+"
+
+${MYSQL_CMD} ssb -e "
+    INSERT INTO dates SELECT * FROM s3('uri' = 's3://yyq-test/regression/ssb/sf100/date.tbl.gz',
+            's3.access_key'= '${S3_AK}',
+            's3.secret_key' = '${S3_SK}',
+            's3.endpoint' = 's3.us-west-2.amazonaws.com',
+            's3.region' = 'us-west-2',
+            'format' = 'csv');
+"
+
+${MYSQL_CMD} ssb -e "
+    INSERT INTO supplier SELECT * FROM s3('uri' = 's3://yyq-test/regression/ssb/sf100/supplier.tbl.gz',
+            's3.access_key'= '${S3_AK}',
+            's3.secret_key' = '${S3_SK}',
+            's3.endpoint' = 's3.us-west-2.amazonaws.com',
+            's3.region' = 'us-west-2',
+            'format' = 'csv');
+"
+
+${MYSQL_CMD} ssb -e "
+    INSERT INTO part SELECT * FROM s3('uri' = 's3://yyq-test/regression/ssb/sf100/part.tbl.gz',
+            's3.access_key'= '${S3_AK}',
+            's3.secret_key' = '${S3_SK}',
+            's3.endpoint' = 's3.us-west-2.amazonaws.com',
+            's3.region' = 'us-west-2',
+            'format' = 'csv');
+"
+
+${MYSQL_CMD} ssb -e "
+    INSERT INTO lineorder SELECT * FROM s3('uri' = 's3://yyq-test/regression/ssb/sf100/lineorder.tbl.*',
+            's3.access_key'= '${S3_AK}',
+            's3.secret_key' = '${S3_SK}',
+            's3.endpoint' = 's3.us-west-2.amazonaws.com',
+            's3.region' = 'us-west-2',
+            'format' = 'csv');
+"
+
+END=$(date +%s)
+LOADTIME=$(echo "$END - $START" | bc)
+echo "Load time: $LOADTIME"
+echo "$LOADTIME" > loadtime
+
+# Dataset contains 99997497 rows, storage size is about 17319588503 bytes
+mysql -vvv -h${VELODB_ENDPOINT} -p${VELODB_PASSWORD} -P${VELODB_PORT} -u${VELODB_USER} ssb -e "SELECT count(*) FROM lineorder" | tee -a log.txt
+du -bs "$DORIS_HOME"/be/storage/ | cut -f1 | tee storage_size
+
+echo "Data size: $(cat storage_size)"
+
+./run.sh 2>&1 | tee -a log.txt
+
+cat log.txt |
+  grep -P 'rows? in set|Empty set|^ERROR' |
+  sed -r -e 's/^ERROR.*$/null/; s/^.*?\((([0-9.]+) min )?([0-9.]+) sec\).*?$/\2 \3/' |
+  awk '{ if ($2 != "") { print $1 * 60 + $2 } else { print $1 } }' |
+  awk '{ if (i % 3 == 0) { printf "[" }; printf $1; if (i % 3 != 2) { printf "," } else { print "]," }; ++i; }'
